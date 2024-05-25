8f019fef36873bd316023ed3ae691ec489d100d7
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-05-25 15:45:44 -0300

    Primer commit

--- src/usuario/Celular.java
+++ src/usuario/Celular.java
@@ -1,5 +1,16 @@
 package usuario;
 
 public class Celular {
-
+     public int numero;
+     private AppUsuario appUsuario;
+     
+     protected Celular() {}
+     
+     protected Celular(int num) {
+    	 this.numero = num;
+    	 this.appUsuario = new AppUsuario();
+     }
+     
+     public int numero() { return this.numero; }
+     
 }
--- src/usuario/Usuario.java
+++ src/usuario/Usuario.java
@@ -1,5 +1,16 @@
 package usuario;
 
 public class Usuario {
-
+	private String patente;
+	private Celular celular;
+	
+	
+	public Usuario() {}
+	
+	public Usuario(String patente, int numTelefono) {
+		this.patente = patente;
+		this.celular = new Celular(numTelefono);
+	}
+	
+	
 }

147169b2ebd1d638acae45b0532dc3f1b161eb4c
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-06-04 19:44:21 -0300

    w

--- src/appUsuario/AppUsuario.java
+++ src/appUsuario/AppUsuario.java
@@ -1,5 +1,29 @@
 package appUsuario;
 
-public class AppUsuario {
-//
+public class AppUsuario implements MovementSensor{
+      private double saldo;
+      private Sem sem;
+      
+      public double saldo() {return saldo;}
+      protected AppUsuario() {}
+      
+      protected AppUsuario(Sem sem) {
+    	  this.saldo = 0;
+    	  this.sem = sem;
+      }
+      
+      public void iniciarEstacionamiento() {
+    	  
+      }
+
+      public void finalizarEstacionamiento() {
+    	  
+      }
+      
+      private void duracion() {
+    	  
+      }
+      
 }
+
+

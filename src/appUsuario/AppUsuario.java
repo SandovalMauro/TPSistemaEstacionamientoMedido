3d9fc605a035e9555c7f7d9e2709bc2e822c18dc
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-06-07 19:30:26 -0300

    AppUsuario absorve Usuario

--- src/appUsuario/AppUsuario.java
+++ src/appUsuario/AppUsuario.java
@@ -5,47 +5,40 @@ import usuario.Celular;
 
 public class AppUsuario implements MovementSensor {
 	private double saldo;
+	private int numero;
 	private SEM sem;
 	private ModoApp modo;
-	private Celular celular;
+	private String patente;
+
+	protected AppUsuario(SEM sem, int numero, String patente) {
+		this.saldo = 0;
+		this.sem = sem;
+		this.modo = new ModoManual();
+		this.numero = numero;
+		this.patente = patente;
+		
+	}
 
 	public double saldo() {
 		return saldo;
 	}
 
-	protected AppUsuario() {
-	}
-
-	protected AppUsuario(SEM sem, Celular celular) {
-		this.saldo = 0;
-		this.sem = sem;
-		this.modo = new ModoManual();
-		this.celular = celular;
-	}
+	
+	
 	
 	public void pasarAAutomatico(){this.modo = new ModoAutomatico();}
 	public void pasarAManual(){this.modo = new ModoManual();}	
 	
-	public void iniciarEstacionamiento() {
-		this.sem.iniciarEstacionamiento(patente);
-	}
+	public void iniciarEstacionamiento() {}
 
-	public void finalizarEstacionamiento() {
-		this.sem.finalizarEstacionamiento(celular.numero());
-	}
+	public void finalizarEstacionamiento() {}
 
-	private void duracion() {
-
-	}
+	private void duracion() {}
 
 	@Override
-	public void driving() {
-		this.modo.driving(this);
-	}
+	public void driving() {}
 
 	@Override
-	public void walking() {
-		this.modo.walking(this);
-	}
+	public void walking() {}
 
 }

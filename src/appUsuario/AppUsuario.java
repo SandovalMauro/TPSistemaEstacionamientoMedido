c4e9af0110587c6061642ad3b1846d346b0cba95
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-06-07 21:21:22 -0300

    cositas de la app jiji

--- src/appUsuario/AppUsuario.java
+++ src/appUsuario/AppUsuario.java
@@ -9,6 +9,8 @@ public class AppUsuario implements MovementSensor {
 	private SEM sem;
 	private ModoApp modo;
 	private String patente;
+	private boolean flagDriving;
+	private boolean sensorActivo;
 
 	protected AppUsuario(SEM sem, int numero, String patente) {
 		this.saldo = 0;
@@ -16,29 +18,43 @@ public class AppUsuario implements MovementSensor {
 		this.modo = new ModoManual();
 		this.numero = numero;
 		this.patente = patente;
-		
+		this.flagDriving = false;
+		this.sensorActivo = false;
 	}
 
-	public double saldo() {
-		return saldo;
-	}
-
+	public double saldo() { return this.saldo; }
 	
-	
+	public void cargarSaldo(double cantidad) { this.saldo += cantidad; }
 	
 	public void pasarAAutomatico(){this.modo = new ModoAutomatico();}
 	public void pasarAManual(){this.modo = new ModoManual();}	
 	
-	public void iniciarEstacionamiento() {}
+	public void iniciarEstacionamiento() {
+		// CAMBIAR PARA QUE SE GENERE ACA Y SE LE MANDE AL SEM
+	}
 
-	public void finalizarEstacionamiento() {}
+	public void finalizarEstacionamiento() {
+		//this.sem.finalizarEstacionamiento(this.numero);
+		// CAMBIAR PARA QUE SE GENERE ACA Y SE LE MANDE AL SEM
+	}
 
-	private void duracion() {}
+	private void duracion() { /*???????????????????????????????????????????*/}
 
 	@Override
-	public void driving() {}
+	public void driving() {
+		if (this.sensorActivo && !this.flagDriving) {
+			this.modo.driving(this); this.flagDriving = true;
+	}   }
 
 	@Override
-	public void walking() {}
+	public void walking() {
+		if (this.sensorActivo && this.flagDriving) {
+			this.modo.walking(this); this.flagDriving = false;
+	}	}
 
+	public void setModoAutomatico() { this.modo = new ModoAutomatico(); this.sensorActivo = true ;}
+	public void     setModoManual() { this.modo = new ModoManual();     this.sensorActivo = false;}	
+	
+	
+	
 }

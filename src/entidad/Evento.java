114a749e026d8a60af0c6888525ddb9e4558bd41
Leo <Leo@DESKTOP-OUNT9UD.fibertel.com.ar>
2024-06-11 13:31:31 -0300

    Creación de test para Entidad, +comments en evento

--- src/entidad/Evento.java
+++ src/entidad/Evento.java
@@ -4,11 +4,15 @@ public class Evento {
     private String tipo;
     private Object datos;
 
+    // Constructor
+    
     public Evento(String tipo, Object datos) {
         this.tipo = tipo;
         this.datos = datos;
     }
 
+    // Getters y Setters
+    
     public String getTipo() {
         return tipo;
     }

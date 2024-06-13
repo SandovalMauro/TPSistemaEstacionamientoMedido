a995d7cdd327729b0ab5a400b628e52bfcc1b799
Leo <Leo@DESKTOP-OUNT9UD.fibertel.com.ar>
2024-06-12 22:15:17 -0300

    Fix error

--- test/inspector/InspectorTest.java
+++ test/inspector/InspectorTest.java
@@ -10,48 +10,47 @@ import sem.Zona;
 
 public class InspectorTest {
 
-    private AppInspector appInspector;
-    private Inspector inspector;
-    private Zona zona;
+	private AppInspector appInspector;
+	private Inspector inspector;
+	private Zona zona;
 
-    @BeforeEach
-    public void setUp() {
-        appInspector = mock(AppInspector.class); 
-        zona = mock(Zona.class);
-        inspector = new Inspector("Jose", 12345, appInspector, zona); 
-    }
+	@BeforeEach
+	public void setUp() {
+		appInspector = mock(AppInspector.class);
+		zona = mock(Zona.class);
+		inspector = new Inspector("Jose", 12345, appInspector, zona);
+	}
 
-    @Test
-    public void testTieneEstacionamientoValido() {
-        when(appInspector.tieneEstacionamientoVigente("abc123")).thenReturn(true);
-        inspector.tieneEstacionamientoValido("abc123");
+	@Test
+	public void testTieneEstacionamientoValido() {
+		when(appInspector.tieneEstacionamientoVigente("abc123")).thenReturn(true);
+		inspector.tieneEstacionamientoValido("abc123");
 
-        verify(appInspector, never()).altaDeInfraccion("abc123");
-        verify(appInspector).tieneEstacionamientoVigente("abc123");
-    }
+		verify(appInspector, never()).altaDeInfraccion("abc123");
+		verify(appInspector).tieneEstacionamientoVigente("abc123");
+	}
 
-    @Test
-    public void testNOTieneEstacionamientoValido() {
-        when(appInspector.tieneEstacionamientoVigente("abc123")).thenReturn(false);
-        inspector.tieneEstacionamientoValido("abc123");
+	@Test
+	public void testNOTieneEstacionamientoValido() {
+		when(appInspector.tieneEstacionamientoVigente("abc123")).thenReturn(false);
+		inspector.tieneEstacionamientoValido("abc123");
 
-        verify(appInspector).altaDeInfraccion("abc123");
-        verify(appInspector).tieneEstacionamientoVigente("abc123");
-    }
-    
-    @Test
-    public void testGetNombreInspector() {
-    	assertEquals("Jose", inspector.getNombreInspector());
-    }
-    
-    @Test
-    public void testGetIDinspector() {
-    	assertEquals(12345, inspector.getIDinspector());
-    }
-    
-    @Test
-    public void testGetZona() {
-    	assertEquals(zona, inspector.getZona());
-    }
+		verify(appInspector).altaDeInfraccion("abc123");
+		verify(appInspector).tieneEstacionamientoVigente("abc123");
+	}
+
+	@Test
+	public void testGetNombreInspector() {
+		assertEquals("Jose", inspector.getNombreInspector());
+	}
+
+	@Test
+	public void testGetIDinspector() {
+		assertEquals(12345, inspector.getIDinspector());
+	}
+
+	@Test
+	public void testGetZona() {
+		assertEquals(zona, inspector.getZona());
+	}
 }
-
